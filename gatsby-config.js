8a9588803ee828a1aedd122c96e50175d4668709
Sam Goddard <hello@samgoddard.co.uk>
2019-09-23 22:33:05 +0100

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-148522557-1",
+        },
+      },
+    }
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
